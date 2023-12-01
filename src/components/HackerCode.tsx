fc993b4ce746cc3640a190841b36a0bcfa7660ee
ihara <ihar.anikeyenka@gmail.com>
2023-12-01 22:01:56 +0100

    fixess

--- src/components/HackerCode.tsx
+++ src/components/HackerCode.tsx
@@ -8,7 +8,7 @@ export const HackerCode = () => {
     const timerRef = useRef<NodeJS.Timeout | null>(null);
     const codeLines = [
         'function data(length) {',
-        '  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";',
+        '  const characters = "abcdefghijklmnopqrstuvwxyz0123456789";',
         '  let result = "";',
         '  for (let i = 0; i < length; i++) {',
         '    result += characters.charAt(Math.floor(Math.random() * characters.length));',
